e7aef883f2c8b6949841c3cf29fbc2fcf08ef177
GalileoHadsi <denismutindagalileo@gmail.com>
2020-06-27 00:47:16 +0200

    Basics working

--- app/src/main/java/com/example/shoppingapp/ChooseItems.java
+++ app/src/main/java/com/example/shoppingapp/ChooseItems.java
@@ -1,13 +1,43 @@
 package com.example.shoppingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseItems extends AppCompatActivity {
+
+    // Unique tag for the intent reply
+    public static final String EXTRA_REPLY = "com.example.shoppingapp.extra.REPLY";
+
+    //  Variable for the item chosen
+    private String userChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_items);
+    }
+
+    public void itemA(View view) {
+        userChoice = "A";
+
+        /*
+            Create a new intent for the reply, add the item chosen to it
+            as an extra, set the new intent result, and close the activity.
+         */
+        Intent itemChosen = new Intent();
+        itemChosen.putExtra(EXTRA_REPLY, userChoice);
+        setResult(RESULT_OK, itemChosen);
+        finish();
+    }
+
+    public void itemB(View view) {
+    }
+
+    public void itemC(View view) {
+    }
+
+    public void itemD(View view) {
     }
 }
--- app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.shoppingapp;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Unique tag required for the intent extra
+    public static final int TEXT_REQUEST = 1;
+
+    // Item to add to TextView1
+    private String itemReply;
+
+    // TextView initialisations
+    private TextView item1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        item1 = findViewById(R.id.item1);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, ChooseItems.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST){
+            // Test to make sure the intent reply result was good
+            if (resultCode == RESULT_OK){
+                itemReply = data.getStringExtra(ChooseItems.EXTRA_REPLY);
+                item1.setText(itemReply);
+            }
+        }
     }
 }
